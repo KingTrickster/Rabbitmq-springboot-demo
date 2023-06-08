c65a88018fc861a9c5a1c8d45ac54a60e44e08f6
Alejandro Trejo <alextrejores@gmail.com>
2023-06-07 22:41:07 -0600

    first commit

---
+++ src/main/java/com/trxjster/rabbitmqspringbootdemo/RabbitmqSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.trxjster.rabbitmqspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqSpringBootDemoApplication.class, args);
+	}
+
+}
